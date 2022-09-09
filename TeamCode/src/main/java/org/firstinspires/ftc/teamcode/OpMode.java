da1603c1408281b1b1485a8eaa437efd4d7765bf
mynameispet <ultraswordkirbypink@gmail.com>
2022-09-09 13:26:57 -0400

    created system and chassis system

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name="Main Loop!")
 public class OpMode extends LinearOpMode {
