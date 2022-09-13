44daa17e6da13f21dd01d0035b1707f3f00b3a27
Lune3x3 <70852368+Lune3x3@users.noreply.github.com>
2022-09-13 16:00:42 -0400

    Update Chassis.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/system/Chassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/system/Chassis.java
@@ -4,14 +4,75 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Chassis extends System{
 
+    DcMotor motorLeft;
+    DcMotor motorRight;
+    ColorSensor color1;
+    DistanceSensor distance1;
+    BNO055IMU imu;
+  
     public Chassis(Telemetry telemetry){
         super(telemetry);
+        
+        motorLeft = hardwareMap.get(DcMotor.class, "motorLeft");
+        motorRight = hardwareMap.get(DcMotor.class, "motorRight");
+        color1  = hardwareMap.get(ColorSensor.class, "color1");
+        distance1  = hardwareMap.get(DistanceSensor.class, "distance1");
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        
+        motorLeft.setDirection("FORWARD");
+        motorRight.setDirection("FORWARD");
 
     }
 
     @Override
     public void update() {
-
+        
+    }
+    
+    public void move(float left, float right){
+        motorLeft.setPower(left);
+        motorRight.setPower(right);
+    }
+    
+    public float checkDistance(DcMotor motor){
+        float value = motor.getCurrentPosition();
+        return value;
     }
 
+    public void goStraight(float distance){
+        // convert
+        float leftDistance = this.checkDistance(this.motorLeft);
+        float rightDistance = this.checkDistance(this.motorRight);
+        
+        float averageDistance = (leftDistance + rightDistance) / 2;
+        
+        while(averageDistance != distance){
+            float left = this.PID(leftDistance, target, 0.1, 0.1, 0.1);
+            float right = this.PID(rightDistance, target, 0.1, 0.1, 0.1);
+            this.move(left, right);
+            
+            // Update
+            float leftDistance = this.checkDistance(this.motorLeft);
+            float rightDistance = this.checkDistance(this.motorRight);
+
+            float averageDistance = (leftDistance + rightDistance) / 2;
+        }
+    }
+    
+    public float PID(float input, float target, float Kp, float Ki, float Kd) {
+        static int prevError = 0, integral = 0;
+
+        double error = (target - input);
+        double derivative = error - prevError;  // only an approximation
+        integral = 0.5 * integral + error;  // only an approximation
+        prevError = error;
+
+        return Kp * error + Kd * derivative + Ki * integral;
+    }
+    
+    public void turn(float degree, float radius){
+        float theta = degree;
+        S = theta * radius;
+        
+    }
 }
