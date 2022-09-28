3de346fac2e6ba4d137bf9ba7050a94d0d7d738b
mynameispet <ultraswordkirbypink@gmail.com>
2022-09-28 19:47:36 -0400

    sept 28, 2022 get ticks from motors - made a wrapper for motors; mainly to manage and store ticks turned and deltaticks

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode.java
@@ -6,8 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp(name="Main Loop!")
 public class OpMode extends LinearOpMode {
 
-    public DcMotor left_motor, right_motor;
-
+    public DcMotor left_motor, right_motor, geartest;
     
 
     @Override
@@ -18,8 +17,17 @@ public class OpMode extends LinearOpMode {
         left_motor = hardwareMap.get(DcMotor.class, "fl");
         right_motor = hardwareMap.get(DcMotor.class, "fr");
 
-        //left_motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //right_motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        left_motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        right_motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        left_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        right_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        left_motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        right_motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // geartest
+        geartest = hardwareMap.get(DcMotor.class, "geartest");
+        geartest.setPower(1.0);
 
 //        int loc = motor.getCurrentPosition();
 
@@ -33,16 +41,14 @@ public class OpMode extends LinearOpMode {
             left_motor.setPower(gamepad1.left_stick_y);
             right_motor.setPower(gamepad1.right_stick_y);
             if (Math.abs(gamepad1.left_stick_y)<=0.1){
-                left_motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 //                left_motor.setPower(iL);
                 iL *= 0.7;
             }
             if (Math.abs(gamepad1.right_stick_y)<=0.1){
-                right_motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 //                right_motor.setPower(iR);
                 iR *= 0.7;
             }
-            telemetry.addData("Power: ", String.format("%f, %f", left_motor.getPower(), right_motor.getPower()));
+            telemetry.addData("Power: ", String.format("%d, %d", left_motor.getCurrentPosition(), right_motor.getCurrentPosition()));
             telemetry.update();
             sleep(50);
         }
