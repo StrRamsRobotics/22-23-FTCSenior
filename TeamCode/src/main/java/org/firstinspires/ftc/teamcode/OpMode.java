347809995b4ea57c5f5336be5da07be78a7ece75
darren10101 <68406815+darren10101@users.noreply.github.com>
2022-09-29 16:17:00 -0400

    changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode.java
@@ -2,28 +2,27 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name="Main Loop!")
 public class OpMode extends LinearOpMode {
 
-    public DcMotor left_motor, right_motor, geartest;
+    public DcMotor left_motor, right_motor, geartest, arm;
     
 
     @Override
     public void runOpMode() throws InterruptedException {
 
-        double iL = 0.0, iR = 0.0;
-
         left_motor = hardwareMap.get(DcMotor.class, "fl");
         right_motor = hardwareMap.get(DcMotor.class, "fr");
+        arm = hardwareMap.get(DcMotor.class, "arm");
 
         left_motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         right_motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         left_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         right_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        left_motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        right_motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        left_motor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // geartest
         geartest = hardwareMap.get(DcMotor.class, "geartest");
@@ -34,20 +33,16 @@ public class OpMode extends LinearOpMode {
 
         waitForStart();
 
-        while (opModeIsActive())
-        {
-//            iL = gamepad1.left_stick_y * -0.4;
-//            iR = gamepad1.right_stick_y * -0.4;
-            left_motor.setPower(gamepad1.left_stick_y);
-            right_motor.setPower(gamepad1.right_stick_y);
-            if (Math.abs(gamepad1.left_stick_y)<=0.1){
-//                left_motor.setPower(iL);
-                iL *= 0.7;
-            }
-            if (Math.abs(gamepad1.right_stick_y)<=0.1){
-//                right_motor.setPower(iR);
-                iR *= 0.7;
-            }
+        while (opModeIsActive()) {
+            double power, turn_power;
+            if (Math.abs(gamepad1.left_stick_y)<=0.1) power = 0;
+            else power = gamepad1.left_stick_y;
+            if (Math.abs(gamepad1.right_stick_x)<=0.1) turn_power = 0;
+            else turn_power = gamepad1.right_stick_x;
+
+            left_motor.setPower(power+turn_power);
+            right_motor.setPower(power-turn_power);
+
             telemetry.addData("Power: ", String.format("%d, %d", left_motor.getCurrentPosition(), right_motor.getCurrentPosition()));
             telemetry.update();
             sleep(50);
