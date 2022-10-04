3bdb7ece2ff74b26d12dc1d7895d3ef8f3545350
vmkcr <vmkcreeper@gmail.com>
2022-10-04 15:56:04 -0400

    it worky

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/system/Vision.java
@@ -0,0 +1,183 @@
+package org.firstinspires.ftc.teamcode.system;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera2;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+@TeleOp(name="hhhhhh")
+public class Vision extends LinearOpMode
+{
+    OpenCvCamera phoneCam;
+
+    @Override
+    public void runOpMode()
+    {
+        /*
+         * cameraMonitorViewId is the monitor we want to display to
+         * phoneCam is an openCV camera object
+         */
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        phoneCam = OpenCvCameraFactory.getInstance().createInternalCamera2(OpenCvInternalCamera2.CameraDirection.BACK, cameraMonitorViewId);
+
+        // no monitor camera view
+        //phoneCam = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK);
+
+        /*
+         * Image processing pipeline = samplePipeline()
+         */
+        phoneCam.setPipeline(new SamplePipeline());
+
+        /*
+         * Connecting to camera
+         */
+        phoneCam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                /*
+                 * Start streaming images, specify resolution and rotation
+                 */
+                phoneCam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });
+
+        telemetry.addLine("Waiting for start");
+        telemetry.update();
+
+        /*
+         * Wait for the user to press start on the Driver Station
+         */
+        waitForStart();
+
+        while (opModeIsActive())
+        {
+            /*
+             * Send some stats to the telemetry
+             */
+            telemetry.addData("Frame Count", phoneCam.getFrameCount());
+            telemetry.addData("FPS", String.format("%.2f", phoneCam.getFps()));
+            telemetry.addData("Total frame time ms", phoneCam.getTotalFrameTimeMs());
+            telemetry.addData("Pipeline time ms", phoneCam.getPipelineTimeMs());
+            telemetry.addData("Overhead time ms", phoneCam.getOverheadTimeMs());
+            telemetry.addData("Theoretical max FPS", phoneCam.getCurrentPipelineMaxFps());
+            telemetry.update();
+
+            /*
+             * Hotkey to stop streaming
+             */
+            if(gamepad1.a)
+            {
+                // Stoping camera
+                phoneCam.stopStreaming();
+                // stop pileline
+                //phoneCam.closeCameraDevice();
+            }
+
+            /*
+             * 10 ticks = 1 hz
+             * stop cpu throttling
+             */
+            sleep(100);
+        }
+    }
+
+    /*
+     * An example image processing pipeline to be run upon receipt of each frame from the camera.
+     * Note that the processFrame() method is called serially from the frame worker thread -
+     * that is, a new camera frame will not come in while you're still processing a previous one.
+     * In other words, the processFrame() method will never be called multiple times simultaneously.
+     *
+     * However, the rendering of your processed image to the viewport is done in parallel to the
+     * frame worker thread. That is, the amount of time it takes to render the image to the
+     * viewport does NOT impact the amount of frames per second that your pipeline can process.
+     *
+     * IMPORTANT NOTE: this pipeline is NOT invoked on your OpMode thread. It is invoked on the
+     * frame worker thread. This should not be a problem in the vast majority of cases. However,
+     * if you're doing something weird where you do need it synchronized with your OpMode thread,
+     * then you will need to account for that accordingly.
+     */
+    public static class SamplePipeline extends OpenCvPipeline
+    {
+        private static final Scalar BLUE = new Scalar(0, 0, 255);
+
+        private static final int THRESHOLD = 107;
+
+        Point topLeft = new Point(50, 50);
+        Point bottomRight = new Point(100, 100);
+
+        Mat region1_Cb;
+        Mat YCrCb = new Mat();
+        Mat Cb = new Mat();
+
+        private volatile int average;
+        private volatile TYPE type = TYPE.BALL;
+
+        private void inputToCb(Mat input) {
+            Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
+            Core.extractChannel(YCrCb, Cb, 2);
+        }
+
+        @Override
+        public void init(Mat input) {
+            inputToCb(input);
+
+            region1_Cb = Cb.submat(new Rect(topLeft, bottomRight));
+        }
+
+        @Override
+        public Mat processFrame(Mat input) {
+            inputToCb(input);
+
+            average = (int) Core.mean(region1_Cb).val[0];
+
+            Imgproc.rectangle(input, topLeft, bottomRight, BLUE, 2);
+
+            if (average > THRESHOLD) {
+                type = TYPE.BALL;
+            } else {
+                type = TYPE.CUBE;
+            }
+
+            return input;
+        }
+
+        public TYPE getType() {
+            return type;
+        }
+
+        public int getAverage() {
+            return average;
+        }
+
+        public enum TYPE {
+            BALL, CUBE
+        }
+            /*
+             * IMPORTANT NOTE: the input Mat that is passed in as a parameter to this method
+             * will only dereference to the same image for the duration of this particular
+             * invocation of this method. That is, if for some reason you'd like to save a copy
+             * of this particular frame for later use, you will need to either clone it or copy
+             * it to another Mat.
+             */
+    }
+}
